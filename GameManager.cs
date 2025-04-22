b0789e2d87000a8b4e4d44fa54be49d0e952bd99
SIW2O25 <sheltie37@naver.com>
2025-04-22 19:30:01 +0900

    player SetPlayerInfo : 수정
    player SetPlayerInfo 수정 및 GameManager SetPlayerInfo 수정.

--- GameManager.cs
+++ GameManager.cs
@@ -13,7 +13,7 @@
         static void Main(string[] args)
         {
             Player player = new Player();
-            player.SetPlayerInfo("보리스", "검사", 15, 1500, 850, 35, 450, 55, 750, 26, 50000);
+            player.SetPlayerInfo(15, "보리스", "검사", 35, 26, 450, 50000);
 
             Inventory inventory = new Inventory();
             inventory.SetInventoryInfo("\\등급(노멀)\n\t\t\\내구도 30/100\n\t\t\\공격력 +5\n\t\t\\데미지 8~16", "\\등급(노멀)\n\t\t\\내구도 80/100\t\n\t\t\\방어력 +15", "\\등급(레어)\t\n\t\t\\내구도 100/100\t\n\t\t\\방어력 + 35", "\\100 회복력", "\\40 회복력");
--- Player.cs
+++ Player.cs
@@ -3,30 +3,22 @@ namespace Player
 {
     public class Player
     {
+        public int level;
         public string name;
         public string job;
-        public int level;
-        public int Hp;
-        public int Mp;
         public int strength;
-        public long demege;
-        public float magicStength;
-        public long magicdemege;
         public int defensive;
+        public int Hp;
         public int money;
 
-        public void SetPlayerInfo(string name, string job, int level, int Hp, int Mp, int strength, long demege, float magicStength, long magicdemege, int defensive, int money)
+        public void SetPlayerInfo(int level, string name, string job, int strength, int defensive, int Hp, int money)
         {
+            this.level = level;
             this.name = name;
             this.job = job;
-            this.level = level;
-            this.Hp = Hp;
-            this.Mp = Mp;
             this.strength = strength;
-            this.demege = demege;
-            this.magicStength = magicStength;
-            this.magicdemege = magicdemege;
             this.defensive = defensive;
+            this.Hp = Hp;
             this.money = money;
         }
 
@@ -38,16 +30,12 @@ namespace Player
             {
                 Console.Clear();
                 Console.WriteLine("캐릭터 정보");
+                Console.WriteLine($"레벨: {level}");
                 Console.WriteLine($"이름: {name}");
                 Console.WriteLine($"직업: {job}");
-                Console.WriteLine($"레벨: {level}");
-                Console.WriteLine($"체력: {Hp}");
-                Console.WriteLine($"마력: {Mp}");
                 Console.WriteLine($"공격력: {strength}");
-                Console.WriteLine($"데미지: {demege}");
-                Console.WriteLine($"마법공격력: {magicStength}");
-                Console.WriteLine($"마법데미지: {magicdemege}");
                 Console.WriteLine($"방어력: {defensive}");
+                Console.WriteLine($"체력: {Hp}");
                 Console.WriteLine($"골드: {money}");
 
                 Console.WriteLine();
